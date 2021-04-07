65550c9d76841378a056c4c13762fdc8b7095b5f
psmith22 <64511508+psmith22@users.noreply.github.com>
2021-04-07 16:43:42 -0400

    Update api_distance_matrix.py

--- api_distance_matrix.py
+++ api_distance_matrix.py
@@ -1,3 +1,9 @@
+"""
+Created on Wed Apr  7 13:34:13 2021
+
+@author: patricksmith
+"""
+
 # importing required libraries 
 import requests, json 
 import urllib.request
@@ -10,9 +16,8 @@ def getKey ():
   return key
 
 # This function initializes the matrix
-def initializeMatrix (): 
-  n=4
-  matrix = [[0 for x in range(n)] for y in range(n)]
+def initializeMatrix (num_locs): 
+  matrix = [[0 for x in range(num_locs)] for y in range(num_locs)]
   return matrix
 
 # This function reads the locations from a file
@@ -23,11 +28,28 @@ def getLocations():
   return locations
 
 # This function converts locations into correct format for API call
-def formatLocations(locations):
-  locations = locations.replace('\n', '|')
-  locations = locations.replace('.', '')
-  locations = locations.replace(',', '')
-  locations = locations.replace(' ', '+')
+def formatLocations(locationsList):
+  n = len(locationsList)
+  locations = ""
+  for i in range(n):
+    temp = str(locationsList[i].addressNumber)
+    temp += " "
+    temp += str(locationsList[i].streetName)
+    temp += " "
+    temp += str(locationsList[i].streetSuffix)
+    temp += " "
+    temp += str(locationsList[i].city) 
+    temp += " "
+    temp += str(locationsList[i].state)
+    temp += " "
+    temp += str(locationsList[i].zipCode)
+    if(i != n-1):
+        temp += "|"
+    temp = temp.replace('\n', '|')
+    temp = temp.replace('.', '')
+    temp = temp.replace(',', '')
+    temp = temp.replace(' ', '+')
+    locations = locations + temp
   return(locations)
 
 
@@ -49,33 +71,39 @@ def callAPI (origin, destination, key):
 
   response = urllib.request.urlopen(url)
   response_json = json.loads(response.read())
+  
   return response_json
 
 # This function creates a distance matrix using times in minutes
-def makeDistMatrix(response_json):
-  matrix = initializeMatrix()
-  for i in range(4):
-    for j in range(4):
+def makeDistMatrix(response_json, num_locs):
+  matrix = initializeMatrix(num_locs)
+  for i in range(num_locs):
+    for j in range(num_locs):
       distance_duration = response_json['rows'][i]['elements'][j]
       matrix[i][j] = distance_duration.get("duration_in_traffic").get("value")/60
       if matrix [i][j] < 0.15:
         matrix[i][j] = 0.0
   return matrix
     
-def apiDistMatrix():  
+def apiDistMatrix(locs):  
+
+  #get number of locations
+  num_locs = len(locs)  
+  
   #get key
   key = getKey()
 
   #get locations, format them, and set to origin and destination
-  locations = getLocations()
-  locations = formatLocations(locations)
+  #locations = getLocations()
+  locations = formatLocations(locs)
   origin, destination = setOriginDestination(locations)
-
+  
   #make API call and format results
   response_json = callAPI(origin, destination, key)
-
+  
   #create distance matrix
-  distance_matrix = makeDistMatrix(response_json)
+  distance_matrix = makeDistMatrix(response_json, num_locs)
 
   #return distance matrix  
   return distance_matrix
+
