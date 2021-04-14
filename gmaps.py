cfef9e9c6acbef42548948328dfe98a674bac060
psmith22 <64511508+psmith22@users.noreply.github.com>
2021-04-14 13:05:37 -0400

    Update gmaps.py

--- gmaps.py
+++ gmaps.py
@@ -1,3 +1,6 @@
+# ordered dropoffs according to our optimized route
+order_list = visitList(X, locs)
+
 # create map visual
 # create a list of coordinates to pass to gmaps
 coord_list = []
@@ -6,18 +9,20 @@ key = getKey()
 
 address = ''
 
+# call geocode api to create convert addresses to coordinates
 for i in range(N):
-    address = str(locs[i].addressNumber)
+    order = order_list[i]
+    address = str(locs[order].addressNumber)
     address += " "
-    address += str(locs[i].streetName)
+    address += str(locs[order].streetName)
     address += " "
-    address += str(locs[i].streetSuffix)
+    address += str(locs[order].streetSuffix)
     address += " "
-    address += str(locs[i].city) 
+    address += str(locs[order].city) 
     address += " "
-    address += str(locs[i].state)
+    address += str(locs[order].state)
     address += " "
-    address += str(locs[i].zipCode)
+    address += str(locs[order].zipCode)
     
     params = {
         'key': key,
@@ -40,18 +45,20 @@ for i in range(N):
 now = datetime.now()
 
 #set start and end to JON
-start = coord_list[0]
-end = coord_list[0]
+start = coord_list[len(coord_list)-1]
+end = coord_list[len(coord_list)-1]
 
 # set waypoints to all other locations
-waypoints = coord_list[1:len(coord_list)]
+waypoints = coord_list[0:len(coord_list)-1]
 
 gmaps.configure(api_key=key)
 #create the map
 fig = gmaps.figure()
 #create the layer
-layer = gmaps.directions.Directions(start, end, waypoints = waypoints, optimize_waypoints=True,
-                                   mode='car',api_key=key,departure_time = now)
+#layer = gmaps.directions.Directions(start, end, waypoints = waypoints, optimize_waypoints=True,
+#                                   mode='car',api_key=key,departure_time = now)
+
+layer = gmaps.directions_layer(start, end, waypoints = waypoints)
 
 #Add the layers
 fig.add_layer(layer)
